fd743d087388a44b3b196fce7a3c92b776be8d97
deliverzd <tomrto52@gmail.com>
2024-02-12 23:48:30 +0700

    add error page

---
+++ app/error.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
+
+export default function Error({
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string };
+    reset: () => void;
+}) {
+    return (
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+            <h1 className="text-2xl font-bold text-primary">มีบางอย่างผิดพลาด!</h1>
+            <button onClick={() => reset()} className="group mt-2 hover:text-red-500 transition">
+                ลองอีกครั้ง
+                <ArrowPathIcon className="w-4 h-4 inline ml-1 group-hover:ml-2 transition-[margin]" />
+            </button>
+        </div>
+    );
+}
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 export default function notFound() {
     return (
-        <div className="absolute top-0 right-0 left-0 bottom-0 bg-white flex flex-col justify-center items-center">
+        <div className="w-full h-screen flex flex-col justify-center items-center">
             <h1 className="text-2xl font-bold text-primary">ไม่พบหน้านี้!</h1>
             <Link href="/" className="group mt-2 hover:text-red-500 transition">
                 กลับสู่หน้าหลัก
-                <ArrowRightIcon className="w-4 h-4 inline group-hover:ml-2 transition-[margin]" />
+                <ArrowRightIcon className="w-4 h-4 inline ml-1 group-hover:ml-2 transition-[margin]" />
             </Link>
         </div>
     );
